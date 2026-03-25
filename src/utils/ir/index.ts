9de39de8bd59fc64748369e1ed3f28d5a0239af1
Luis Eduardo <eduardo.devop@gmail.com>
2026-03-25 18:11:15 +0000

    refactor: remove unnecessary documentation comments from IR utilities

--- src/utils/ir/index.ts
+++ src/utils/ir/index.ts
@@ -1,19 +1,3 @@
-/**
- * IR helper entry point for annotation lookup, literal unwrapping, and
- * anonymous-type hoisting utilities.
- *
- * @example
- * ```ts
- * import { ir } from "@varavel/vdl-plugin-sdk/utils";
- *
- * const annotation = ir.getAnnotation(field.annotations, "default");
- * const value = annotation?.argument
- *   ? ir.unwrapLiteral(annotation.argument)
- *   : undefined;
- * const flatSchema = ir.hoistAnonymousTypes(schema);
- * ```
- */
-
 export * from "./get-annotation";
 export * from "./get-annotation-arg";
 export * from "./hoist-anonymous-types";
